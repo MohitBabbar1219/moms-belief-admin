712b82933209189262ae51a94f07cc5d8705b9f2
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-14 20:34:26 +0530

    Add mongo db

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+const db = require('./src/config/keys').mongoURI;
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('successfully connected to database...'))
+  .catch((err) => console.log('error occurred while connecting to database', err));
 
 const PORT = process.env.PORT || 4000;
 
---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:admin123@ds135068.mlab.com:35068/moms-belief',
+  secret: 'h847her87hes8g42jkdnsajkudsh98h298hdsaoij92d',
+};
