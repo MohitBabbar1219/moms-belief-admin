837d3639a486f57d1ccbd979da5a87929f5704bd
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-14 23:15:02 +0530

    Remove console log

--- src/helpers/file_upload.js
+++ src/helpers/file_upload.js
@@ -5,7 +5,6 @@ const storage = multer.diskStorage({
     cb(null, 'public/images/uploads')
   },
   filename: (req, file, cb) => {
-    console.log(JSON.stringify(file));
     const fileNameAndExtension = file.originalname.split('.');
     cb(null, `${Date.now()}-${fileNameAndExtension[0]}.${fileNameAndExtension[fileNameAndExtension.length - 1]}`)
   }
