5883a938ac3179f6fe99867d253ca0de43ce3776
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-14 20:19:46 +0530

    Add body-parser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 4000;
 
-app.get('/hello', (req, res) => res.json({msg: "hello"}));
+app.post('/hello', (req, res) => res.json({msg: req.body.data.msg}));
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
