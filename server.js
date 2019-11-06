2e608bf0a75864af9aa660cf7fc5d3dc3c2474a7
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-11-06 22:39:22 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
