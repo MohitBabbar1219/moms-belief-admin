a06441a2b2f99e1418549ad122d90925e590825e
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-11-06 23:01:28 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
